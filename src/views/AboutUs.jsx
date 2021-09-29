ca49f25ad2b92aef2d4a1ec44ebb9ac0bcb49a56
don21395 <don.ng21395@gmail.com>
2021-09-29 19:56:47 +0800

    Update AboutUs.jsx

--- src/views/AboutUs.jsx
+++ src/views/AboutUs.jsx
@@ -4,20 +4,19 @@ class AboutUsView extends Component {
   render() {
     return (
       <React.Fragment>
-        <li></li>
+
         <h1>About Us</h1>
         <p>
           <li>At XXX we believe in the power of the community. We strive to be a platform that encourage the safe exchange of goods and services among</li>
           <li>individuals with various interests.</li>
           
-          <li></li>
+
         </p>
         <h2>Creating 2nd hand market for goods</h2>
         <p>
           <li>One man's trash is another man's treasure. The availability of a 2nd Hand market for goods ensures the reduction of</li>
           <li>waste materials being disposed, thereby alleviating land pollution as a result of waste disposal.</li>
           
-          <li></li>
         </p>
 
         <h2>Services and knowledge exchange</h2>
@@ -25,14 +24,12 @@ class AboutUsView extends Component {
           <li>Besides goods being sold and exchanged, services can also be provided by our users to each other.</li>
           <li>With no added starting cost to the sellers, our platform serves as an easy way to get your business rolling!</li>
           
-          <li></li>
         </p>
         <h2>Safe and available platform</h2>
         <p>
           <li>Our team consistently provided 24/7 support to users 365 days a year to ensure you get the best service.</li>
           <li>Your private information is kept secured by us, so you can rest easy.</li>
           
-          <li></li>
         </p>
       </React.Fragment>
     );
